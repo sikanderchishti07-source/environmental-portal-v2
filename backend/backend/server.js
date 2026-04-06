d7f7a381a13e4396c336aaf67fc80e747e61c02b
sikanderchishti07-source <sikanderchishti07@gmail.com>
2026-04-06 16:47:03 +0300

    Update server.js

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -23,7 +23,7 @@ app.use(cors({
     process.env.FRONTEND_ORIGIN || 'http://localhost',
     'http://localhost:3000',
     'http://localhost:5500',
-    'http://127.0.0.1:5500'
+    'http://127.0.0.1:5500',
     'https://environmental-portal-v2.vercel.app',
   ],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
